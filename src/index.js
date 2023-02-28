3628b6c7d88d599c095ac41991bc0b87e43810f2
ywilfredo <74474078+ywilfredo@users.noreply.github.com>
2023-02-28 18:55:21 -0400

    agregar mongodb-connection a index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express'); //requerir el modulo express
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express(); //ejrcutar express
 const port = process.env.PORT || 9000;
 
 // routes
 app.get("/", (req, res) => {
     res.send("Welcome to my API");
-    });
+});
+
+//mongodb connection
+mongoose
+.connect(process.env.MONGODB_URI)
+.then (() => console.log("Conectado a MongoDB Atlas"))
+.catch((error) => console.error(error));
 
 app.listen(port, () => console.log('server listening on port', port));
 
